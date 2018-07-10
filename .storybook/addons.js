4158c7c81e1fb8c01e1c74c0d7223fc6ef00002c
adnan1naeem <adnan.naeem@blueeast.com>
2018-07-10 11:25:15 +0500

    fix(added addons in storybook): added addons in storybook
    added addons in storybook

--- .storybook/addons.js
+++ .storybook/addons.js
@@ -1,3 +1,9 @@
 import '@storybook/addon-actions/register';
 import '@storybook/addon-links/register';
 import '@storybook/addon-knobs/register'
+import '@storybook/addon-console';
+import '@storybook/addon-viewport/register'
+import 'storybook-readme/register';
+import 'storybook-addon-intl/register';
+import 'storybook-chrome-screenshot/register';
+import 'storybook-addon-specifications/register';
